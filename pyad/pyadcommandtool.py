2dbc8326bdabf6a2b5ab51cf27fc8ca757a64945
Despatcher <ewanmcmahon@gmail.com>
2014-08-21 11:42:58 +0100

    Pyad command tool
    Despatcher setup+sync, basic pyad command extractor/writer.

---
+++ pyad/pyadcommandtool.py
@@ -0,0 +1,14 @@
+__author__ = 'ewanm_000'
+import sys
+import pydoc
+import pyad
+
+def output_help_to_file(filepath, request):
+    f = file(filepath, 'w')
+    sys.stdout = f
+    pydoc.help(pyad)
+    f.close()
+    sys.stdout = sys.__stdout__
+    return
+
+output_help_to_file(r'pyadcommands.txt', 're')
